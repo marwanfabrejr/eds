62e02631adb63e1e1695a3d3032444275f81c965
Marwan Zayed <84870483+marwanfabrejr@users.noreply.github.com>
2024-03-07 13:38:32 +0200

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,4 +3,16 @@ export default function decorate(block) {
   overlayDiv.classList.add('overlay');
   var heroDiv = document.querySelector('.hero');
   heroDiv.appendChild(overlayDiv);
+  var heroH = document.getElementById('this-is-a-headline');
+  var heroP = document.createElement('p');
+  overlayDiv.classList.add('heroP');
+  heroP.innerHTML = 'Subheading · All Breakpoints';
+  var nextSibling = heroH.nextSibling;
+  if (nextSibling) {
+    referenceElement.parentNode.insertBefore(heroP, nextSibling);
+    console.log(heroDiv);
+  } else {
+    referenceElement.parentNode.appendChild(heroP);
+    console.log(heroDiv);
+  }
 }
