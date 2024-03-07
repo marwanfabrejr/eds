9224e84a032ebfa1821e437b9af9c76bd61751c2
Marwan Zayed <84870483+marwanfabrejr@users.noreply.github.com>
2024-03-07 12:21:44 +0200

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
   const overlay = document.createElement('div');
-  const heroDiv = document.document.querySelector('.hero');
+  const heroDiv = document.querySelector('.hero');
   console.log(heroDiv);
 }
