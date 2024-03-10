3eb36757e729fda56ec7085b53cde2956c82554a
Marwan Zayed <84870483+marwanfabrejr@users.noreply.github.com>
2024-03-10 09:54:26 +0200

    updates

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -15,4 +15,29 @@ export default function decorate(block) {
       }
     });
   });
+  var header = document.getElementById('header');
+  var previousElement = header.previousElementSibling;
+  previousElement.classList.add('subtitle');
+  var nextElement = header.nextElementSibling;
+  nextElement.classList.add('paragraph');
+  var parentDiv = header.parentElement;
+  parentDiv.classList.add('desc');
+  var grandParentDiv = parentDiv.parentElement;
+  grandParentDiv.classList.add('columnWrapper');
+  var buttonElement = document.querySelector('[title="Button"]');
+  var buttonParent = buttonElement.parentElement;
+  buttonParent.classList.add('imageWrapper');
+  var header1 = document.getElementById('header-1');
+  var previousElement = header1.previousElementSibling;
+  previousElement.classList.add('subtitle');
+  var nextElement = header1.nextElementSibling;
+  nextElement.classList.add('paragraph');
+  var parentDiv = header1.parentElement;
+  parentDiv.classList.add('desc');
+  var grandParentDiv = parentDiv.parentElement;
+  grandParentDiv.classList.add('columnWrapper');
+  var buttonElement = document.querySelector('[title="Button"]');
+  var buttonParent = buttonElement.parentElement;
+  buttonParent.classList.add('imageWrapper');
+  buttonElement.classList.add('btn');
 }
