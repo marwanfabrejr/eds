2b77b3f56a1ff213c81371d7df55d9561bd47397
Marwan Zayed <84870483+marwanfabrejr@users.noreply.github.com>
2024-03-27 13:18:19 +0200

    Update columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -86,7 +86,10 @@ export default function decorate(block) {
           secondH2.textContent = '-';
           if (answerDiv.style.display === 'none') {
             answerDiv.style.display = 'block';
-          } else answerDiv.style.display = 'none';
+          } else {
+            answerDiv.style.display = 'none';
+            secondH2.textContent = '+';
+          }
         });
       });
 
